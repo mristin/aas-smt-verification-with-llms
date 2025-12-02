04bc2d6be5aa8efff9f7a4e60119f3c052a6af30
s-heppner <iat@s-heppner.com>
2025-12-02 10:49:15 +0100

    Fix CI pipeline errors for evaluation scripts (#19)
    Previously, some codestyle (black), typing (mypy) and more style (pylint) issues
    were present in the evaluation scripts. This fixes them, without changing the
    code logic (which I didn't check).

--- dev_scripts/evaluate_output.py
+++ dev_scripts/evaluate_output.py
@@ -1,44 +1,73 @@
+"""
+This evaluates the output of our experiments.
+"""
+
 import pathlib
 import sys
 import os
 import argparse
 import json
 from dataclasses import dataclass
+from typing import List, Any
 
 
 @dataclass
 class Stats:
+    """
+    A class collecting stats for reporting.
+    """
+
     expected_accepted: int = 0  # This is good :)
     expected_rejected: int = 0
     unexpected_accepted: int = 0
     unexpected_rejected: int = 0  # This is good :)
     invalid_json: int = 0
 
-    def dump(self):
-        def percent(v: float):
+    def dump(self) -> None:
+        """
+        Dump the Stats to a file for persistence
+        """
+
+        def percent(v: float) -> str:
+            """
+            Translate a float between 0 and 1 to a string percentage
+            """
             return f"{v*100:.0f}%"
-        total = self.expected_accepted + self.expected_rejected + self.unexpected_accepted + self.unexpected_rejected + self.invalid_json
+
+        total = (
+            self.expected_accepted
+            + self.expected_rejected
+            + self.unexpected_accepted
+            + self.unexpected_rejected
+            + self.invalid_json
+        )
         if total == 0:
             print("No stats available")
             return
 
         print(f"Invalid json:            {self.invalid_json} of {total} files")
-        print(f"Expected and accepted:   {percent(self.expected_accepted / (self.expected_accepted + self.expected_rejected))}")
-        print(f"Unexpected and rejected: {percent(self.unexpected_rejected / (self.unexpected_accepted + self.unexpected_rejected))}")
+        print(
+            f"Expected and accepted:   "
+            f"{percent(self.expected_accepted / (self.expected_accepted + self.expected_rejected))}"
+        )
+        ratio = self.unexpected_rejected / (
+            self.unexpected_accepted + self.unexpected_rejected
+        )
+        print(f"Unexpected and rejected: {percent(ratio)}")
 
 
-def _load(response_file: pathlib.Path) -> list:
+def _load(response_file: pathlib.Path) -> List[Any]:
     with open(response_file, "rb") as f:
         try:
             data = json.load(f)
         except json.JSONDecodeError as e:
-            raise ValueError(str(e))
+            raise ValueError(str(e)) from e
     if not isinstance(data, list):
         raise ValueError("does not contain a list")
     return data
 
 
-def _evaluate(experiment_dir: pathlib.Path, stats: Stats):
+def _evaluate(experiment_dir: pathlib.Path, stats: Stats) -> None:
     print(f"Evaluating {experiment_dir.name}")
 
     expected_dir = experiment_dir / "expected"
@@ -73,7 +102,11 @@ def _evaluate(experiment_dir: pathlib.Path, stats: Stats):
         else:
             stats.unexpected_rejected += 1
 
-def main():
+
+def main() -> int:
+    """
+    Main method
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="gpt-5-2025-08-07")
     args = parser.parse_args()
